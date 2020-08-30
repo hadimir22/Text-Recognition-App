aff87c48c8c6def96fd1432039220bda91ded5ce
hadimir22 <hadimir22@gmail.com>
2020-08-30 12:26:03 +0530

    feat: text displayed in scrollview

--- lib/ui/showText.dart
+++ lib/ui/showText.dart
@@ -14,6 +14,41 @@ class ShowText extends StatelessWidget {
         backgroundColor: Color(0XFF00BFA6),
       ),
       backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Expanded(
+            flex: 10,
+            child: SingleChildScrollView(
+              child: Container(
+                alignment: Alignment.topLeft,
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  extractedText,
+                  style: TextStyle(fontSize: 50.0),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0.2, 0.0, 0.2, 10.0),
+              child: ListTile(
+                  title: RaisedButton(
+                      splashColor: Colors.black26,
+                      disabledColor: Colors.grey,
+                      elevation: 15.0,
+                      onPressed: () {},
+                      color: Color(0XFF06A48F),
+                      textColor: Colors.white70,
+                      child: Text(
+                        "copy to clipboard",
+                      ))),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
